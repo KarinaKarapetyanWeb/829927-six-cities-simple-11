464916d01be7a45eef81467b132ebe4155717d5e
Karina <karina_karapetyan@mail.ru>
2023-01-19 19:02:57 +0400

    added action

--- project/src/components/sort/sort.tsx
+++ project/src/components/sort/sort.tsx
@@ -24,6 +24,7 @@ function Sort(): JSX.Element {
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
+      {' '}
       <span className="places__sorting-type" tabIndex={0} onClick={handleOpenSortBtnClick}>
         {sort}
         <svg className="places__sorting-arrow" width="7" height="4">
